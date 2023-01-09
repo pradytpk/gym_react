8375401e420e3c1c0b4e1be90cf434041c722be7
Pradeep Kumar <61448614+pradytpk@users.noreply.github.com>
2023-01-09 17:48:54 +0900

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import { Box, Stack, Typography } from '@mui/material';
+import React from 'react';
+import Logo from '../assets/images/Logo-1.png';
+const Footer = () => {
+  return (
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+        <img src={Logo} alt='logo' width='200px' height='40px' />
+        <Typography variant='h5' pb='40px' mt='20px' alignItems='center'>
+        Made with the learning from JavaScript Mastery Youtube Channel.
+      </Typography>
+      </Stack>
+      
+    </Box>
+  );
+};
+
+export default Footer;
